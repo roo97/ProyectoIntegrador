20279cff11977cbbd2cee9d7a5fbad7c84291bbd
roo97 <ruvc9719@gmail.com>
2018-11-16 00:40:22 -0500

    Validaciones Modelo Usuario

--- ProyectoEcommerceModelos/Usuario.cs
+++ ProyectoEcommerceModelos/Usuario.cs
@@ -24,10 +24,31 @@ namespace ProyectoEcommerceModelos
 
 
 
-        /*public void validar()
+        public void validar()
         {
-            throw new NotImplementedException();
-        }*/
+            if (string.IsNullOrEmpty(nomUsu))
+                throw new Exception("Debe colocar nombre de usuario");
+            if (string.IsNullOrEmpty(apePatUsu))
+                throw new Exception("Debe colocar apellido paterno del usuario");
+            if (string.IsNullOrEmpty(apeMatUsu))
+                throw new Exception("Debe colocar apellido materno del usuario");
+            if (string.IsNullOrEmpty(dniUsu))
+                throw new Exception("Debe colocar dni del usuario ");
+            if (dniUsu.Length != 8)
+                throw new Exception("Debe contener 8 caracteres el DNI del usuario");
+            if (string.IsNullOrEmpty(celUsu))
+                throw new Exception("Debe colocar el celular o telefono del usuario ");
+            if (celUsu.Length != 9 || celUsu.Length !=7)
+                throw new Exception("Debe colocar un celular o telefono valido");
+            if (string.IsNullOrEmpty(nickUsu))
+                throw new Exception("Debe colocar el nick del usuario ");
+            if (string.IsNullOrEmpty(pwdUsu))
+                throw new Exception("Debe colocar la contraseña del usuario  ");
+            if (string.IsNullOrEmpty(codTipoUsu.ToString()))
+                throw new Exception("Debe seleccionar un tipo de usuario");
+            if (string.IsNullOrEmpty(mailUsu))
+                throw new Exception("Debe colocar el correo del usuario");
+        }
 
     }
 }
